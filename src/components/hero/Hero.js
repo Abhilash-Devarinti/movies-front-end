d1077724dc80915d3f46a1caf3d6f2b9f5e97611
Abhilash-Devarinti <devarinti.a@northeastern.edu>
2024-05-04 13:49:40 -0500

    Added background Image and implemented the styles

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,13 +6,13 @@ import { Paper } from "@mui/material";
 const Hero = ({movies}) => {
     return (
         <div>
-            <Carousel>
+            <Carousel className="movie-carousel-container">
                 {
                     movies.map((movie) => {
                         return (
                             <Paper>
                                 <div className="movie-card-container">
-                                    <div className="movie-card">
+                                    <div className="movie-card" style={{"--img" : `url(${movie.backdrops[0]})`}}>
                                         <div className="movie-details">
                                             <div className="movie-poster">
                                                 <img src = {movie.poster} />
